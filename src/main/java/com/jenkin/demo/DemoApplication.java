d78f77215e0654ea76eae7490030ec0a24978b75
Naseer <naseer.ahmad@avanzasolutions.com>
2026-06-22 18:18:19 +0500

    testing pipline1

--- src/main/java/com/jenkin/demo/DemoApplication.java
+++ src/main/java/com/jenkin/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+//adding commet to test pipline
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
