1b8ae60bbc2bcf86e747887ffd351905703b385e
Naseer <naseer.ahmad@avanzasolutions.com>
2026-06-22 18:46:24 +0500

    Created a controller

--- src/main/java/com/jenkin/demo/DemoApplication.java
+++ src/main/java/com/jenkin/demo/DemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//updated comments to test pipline
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
